772bc5854300fa06491824c5e8c7face5f703518
daima2018 <1138393283@qq.com>
2021-01-09 11:25:59 +0800

    b

--- ymx-offline/src/main/shell/local-child.sql
+++ ymx-offline/src/main/shell/local-child.sql
@@ -93,61 +93,61 @@ C:
            ,ttt1.currency_local
 
 D:
-select                                                                                                          
-     c.user_account                                           -- '店铺账号'                                                                                                   
-    ,c.site                                                   --  '站点'                                                                                          
+select                                                                       
+     c.user_account                                           -- '店铺账号'                
+    ,c.site                                                   --  '站点'                      
     ,c.seller_sku                                             --
     ,c.parent_asin            --在结果集可以去掉该字段
-    ,c.asin                                                   --                                                                                            
-    ,d.sale_amount as qty                                     -- '销售数量',                                                                                              
-    ,'' as summary_date                                           -- '统计日期',                                                                                                    
+    ,c.asin                                                   --                     
+    ,d.sale_amount as qty                                     -- '销售数量',              
+    ,'' as summary_date                                           -- '统计日期',            
     ,nvl(case when local_rate=1 then floor(100*d.sale_money_original)/100
               else floor(100*d.sale_money_original/c.local_rate)/100
-         end,0) as sale_amount                                                      -- '销售额(本位币)',                                                                                                   
-    ,nvl(floor(100*d.sale_money_original/c.usd_rate)/100,0) as  sale_amount_usd     -- '销售额美元',                                                                                                       
-    ,nvl(floor(100*d.sale_money_original/c.eur_rate)/100,0) as  sale_amount_eur     -- '销售额欧元',                                                                                                       
-    ,nvl(floor(100*d.sale_money_original/c.gbp_rate)/100,0) as  sale_amount_gbp     -- '销售额英镑',                                                                                                       
-    ,nvl(floor(100*d.sale_money_original/c.jpy_rate)/100,0) as  sale_amount_jpy     -- '销售额日元',                                                                                                       
-    ,nvl(d.sale_money_original,0) as sale_amount_original                           -- '销售额（站点原始金额）',                                                                                                            
-    ,date_format(current_timestamp(),'yyyy-MM-dd HH:mm:ss') as created_time         -- '创建时间',                                                                                                    
-    ,date_format(current_timestamp(),'yyyy-MM-dd HH:mm:ss') as updated_time         -- '更新时间',                                                                                                    
-    ,d.sale_order_num                                                               -- '订单数',                                                                                                      
-    ,f.refund_amount                                                                -- '退款listing个数',                                                                                                     
-    ,nvl(floor(100*f.refund_money)/100           ,0)                                -- '退款金额(站点币种)',                                                                                                    
-    ,nvl(floor(100*f.refund_money/c.usd_rate)/100,0) as refund_money_usd            -- '退款金额美元',                                                                                                        
-    ,nvl(floor(100*f.refund_money/c.eur_rate)/100,0) as refund_money_eur            -- '退款金额欧元',                                                                                                        
-    ,nvl(floor(100*f.refund_money/c.gbp_rate)/100,0) as refund_money_gbp            -- '退款金额英镑',                                                                                                        
-    ,nvl(floor(100*f.refund_money/c.jpy_rate)/100,0) as refund_money_jpy            -- '退款金额日元',                                                                                                        
+         end,0) as sale_amount                                                      -- '销售额(本位币)',          
+    ,nvl(floor(100*d.sale_money_original/c.usd_rate)/100,0) as  sale_amount_usd     -- '销售额美元',              
+    ,nvl(floor(100*d.sale_money_original/c.eur_rate)/100,0) as  sale_amount_eur     -- '销售额欧元',               
+    ,nvl(floor(100*d.sale_money_original/c.gbp_rate)/100,0) as  sale_amount_gbp     -- '销售额英镑',               
+    ,nvl(floor(100*d.sale_money_original/c.jpy_rate)/100,0) as  sale_amount_jpy     -- '销售额日元',               
+    ,nvl(d.sale_money_original,0) as sale_amount_original                           -- '销售额（站点原始金额）',   
+    ,date_format(current_timestamp(),'yyyy-MM-dd HH:mm:ss') as created_time         -- '创建时间',            
+    ,date_format(current_timestamp(),'yyyy-MM-dd HH:mm:ss') as updated_time         -- '更新时间',            
+    ,d.sale_order_num                                                               -- '订单数',              
+    ,f.refund_amount                                                                -- '退款listing个数',       
+    ,nvl(floor(100*f.refund_money)/100           ,0)                                -- '退款金额(站点币种)',      
+    ,nvl(floor(100*f.refund_money/c.usd_rate)/100,0) as refund_money_usd            -- '退款金额美元',          
+    ,nvl(floor(100*f.refund_money/c.eur_rate)/100,0) as refund_money_eur            -- '退款金额欧元',          
+    ,nvl(floor(100*f.refund_money/c.gbp_rate)/100,0) as refund_money_gbp            -- '退款金额英镑',          
+    ,nvl(floor(100*f.refund_money/c.jpy_rate)/100,0) as refund_money_jpy            -- '退款金额日元',          
     ,nvl(case when local_rate=1 then floor(100*f.refund_money)/100
               else floor(100*f.refund_money/c.local_rate)/100
-         end,0) as refund_money_local                                              -- '退款金额(本位币)',                                                                                                          
-    ,md5(concat(c.user_account,c.asin,c.seller_sku)) as key1                       -- 'user_account+asin+seller_sku 的MD5值',                                                                                            
-    ,g.return_amount                                                               -- '退货数',                                                                                                     
-    ,c.asin_type                                                                   -- '0未确定, 子asin:1，独立产品:2, 父asin3',                                                                                                 
-    ,e.ad_sale_amount as ad_qty                                                    -- '广告销售数量',                                                                                              
+         end,0) as refund_money_local                                              -- '退款金额(本位币)',                            
+    ,md5(concat(c.user_account,c.asin,c.seller_sku)) as key1                       -- 'user_account+asin+seller_sku 的MD5值',           
+    ,g.return_amount                                                               -- '退货数',                                    
+    ,c.asin_type                                                                   -- '0未确定, 子asin:1，独立产品:2, 父asin3',       
+    ,e.ad_sale_amount as ad_qty                                                    -- '广告销售数量',                             
     ,nvl(case when local_rate=1 then floor(100*e.ad_sale_money_original)/100
               else floor(100*e.ad_sale_money_original/c.local_rate)/100
-         end,0) as ad_sale_amount                                                         -- '广告销售额(本位币)',                                                                                                      
-    ,nvl(floor(100*e.ad_sale_money_original/c.usd_rate)/100,0) as ad_sale_amount_usd      -- '广告销售额美元',                                                                                                          
-    ,nvl(floor(100*e.ad_sale_money_original/c.eur_rate)/100,0) as ad_sale_amount_eur      -- '广告销售额欧元',                                                                                                          
-    ,nvl(floor(100*e.ad_sale_money_original/c.gbp_rate)/100,0) as ad_sale_amount_gbp      -- '广告销售额英镑',                                                                                                          
-    ,nvl(floor(100*e.ad_sale_money_original/c.jpy_rate)/100,0) as ad_sale_amount_jpy      -- '广告销售额日元',                                                                                                          
-    ,nvl(e.ad_sale_order_num,0)                                as ad_sale_order_num       -- '广告订单数',                                                                                                         
-    ,nvl(e.ad_sale_money_original,0)                           as ad_sale_amount_original -- '广告销售额（站点原始金额）',                                                                                                               
-    ,nvl(e.cost,0)                                             as cost                    -- '广告花费(站点币种)',                                                                                            
+         end,0) as ad_sale_amount                                                         -- '广告销售额(本位币)',               
+    ,nvl(floor(100*e.ad_sale_money_original/c.usd_rate)/100,0) as ad_sale_amount_usd      -- '广告销售额美元',                   
+    ,nvl(floor(100*e.ad_sale_money_original/c.eur_rate)/100,0) as ad_sale_amount_eur      -- '广告销售额欧元',                   
+    ,nvl(floor(100*e.ad_sale_money_original/c.gbp_rate)/100,0) as ad_sale_amount_gbp      -- '广告销售额英镑',                   
+    ,nvl(floor(100*e.ad_sale_money_original/c.jpy_rate)/100,0) as ad_sale_amount_jpy      -- '广告销售额日元',                   
+    ,nvl(e.ad_sale_order_num,0)                                as ad_sale_order_num       -- '广告订单数',                  
+    ,nvl(e.ad_sale_money_original,0)                           as ad_sale_amount_original -- '广告销售额（站点原始金额）',           
+    ,nvl(e.cost,0)                                             as cost                    -- '广告花费(站点币种)',          
     ,nvl(case when local_rate=1 then floor(100*e.cost)/100
               else floor(100*e.cost/c.local_rate)/100
-         end,0) as cost_local                                              -- '广告花费(本位币)',                                                                                                  
-    ,nvl(floor(100*e.cost/c.usd_rate)/100,0) as cost_usd                                                                                                                
-    ,nvl(floor(100*e.cost/c.eur_rate)/100,0) as cost_eur                                                                                                               
-    ,nvl(floor(100*e.cost/c.gbp_rate)/100,0) as cost_gbp                                                                                                               
-    ,nvl(floor(100*e.cost/c.jpy_rate)/100,0) as cost_jpy                                                                                                               
-    ,nvl(e.clicks     ,0) as clicks                           -- '广告访问次数,也即广告点击数'                                                                                             
-    ,nvl(e.impressions,0) as impressions                      -- 广告曝光量                                                                                                  
-    ,nvl(h.sessions             ,0) as sessions                 -- 访客次数                                                                                               
-    ,nvl(h.page_views           ,0) as page_views               --浏览次数                                                                                                  
-    ,nvl(h.buy_box_percentage   ,0) as buy_box_percentage       --  buy_box_percentage                                                                                                        
-    ,nvl(h.session_percentage   ,0) as session_percentage       --买家访问次数比率                                                                                                          
+         end,0) as cost_local                                              -- '广告花费(本位币)',       
+    ,nvl(floor(100*e.cost/c.usd_rate)/100,0) as cost_usd                                            
+    ,nvl(floor(100*e.cost/c.eur_rate)/100,0) as cost_eur                                           
+    ,nvl(floor(100*e.cost/c.gbp_rate)/100,0) as cost_gbp                                           
+    ,nvl(floor(100*e.cost/c.jpy_rate)/100,0) as cost_jpy                                           
+    ,nvl(e.clicks     ,0) as clicks                           -- '广告访问次数,也即广告点击数'           
+    ,nvl(e.impressions,0) as impressions                      -- 广告曝光量                              
+    ,nvl(h.sessions             ,0) as sessions                 -- 访客次数                           
+    ,nvl(h.page_views           ,0) as page_views               --浏览次数                              
+    ,nvl(h.buy_box_percentage   ,0) as buy_box_percentage       --  buy_box_percentage           
+    ,nvl(h.session_percentage   ,0) as session_percentage       --买家访问次数比率             
     ,nvl(h.page_views_percentage,0) as page_views_percentage    --   浏览次数比率        
 from C c 
 left join (SELECT
